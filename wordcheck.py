108d58a63d5760dc90b24c5c7bb156019c5b903c
Michael D. Hoyle <hoylemd@gmail.com>
2012-07-09 22:56:53 -0400

    Initial commit

---
+++ wordcheck.py
@@ -0,0 +1,13 @@
+words = ["paladin", "batman", "orange", "salad", "Jaime", "range", "sword", "zebra", "the", "a"]
+
+class Word:
+	def __init(self):
+		self.word = ""
+
+class Language:
+	alphabet = ""
+	
+	def AddWord(word):
+		
+		
+
